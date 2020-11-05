4c20277a5cf48664aa6fcb6b535d60f52e020e53
vitavl6686 <vitalija-vlasova@rambler.ru>
2020-11-05 20:02:03 +0000

    decision_trees_part_1

---
+++ 2_charter/decision_tree.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+new_data = pd.read_csv('titanic.csv', index_col = 'PassengerId')
+
+#Оставим в выборке только нужные нам признаки
+test_data = new_data.filter(items = ['Pclass', 'Fare', 'Age', 'Sex', 'Survived'])
+#выкинем объекты с недостаточными данными
+without_NaN = test_data.dropna()
+#Пол -- непонятная срока. Заменяем его на бин.характеристику
+_valid_test_data = without_NaN.replace('male', 1).replace('female',0)
+
+valid_test_data = _valid_test_data.filter(items = ['Pclass', 'Fare', 'Age', 'Sex'])
+
+answers = _valid_test_data.filter(items = ['Survived'])
+
+clf = DecisionTreeClassifier()
+#Обучаем решающее дерево на нашей выборке
+clf.fit(valid_test_data,answers)
+#Посмотрим наши клевые важнсти признаков
+importances = clf.feature_importances_
+#print(importances)
